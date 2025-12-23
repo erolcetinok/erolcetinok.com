06f9edbb6aeae174df3fcf9bbb6e3787488ae6a1
erolcetinok <erol.cetinok@gmail.com>
2025-12-22 21:20:31 -0800

    adjustment of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main style={{ padding: 40, fontFamily: "system-ui" }}>
+    <>
       <h1>Erol Cetinok</h1>
       <p>My Portfolio</p>
-    </main>
+    </>
   );
 }
