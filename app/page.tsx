eea4aa78a3f3ed3aa9409f91f9a06f9ef8eec62b
erolcetinok <erol.cetinok@gmail.com>
2025-12-23 16:04:30 -0800

    added italicized links

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
 export default function Home() {
   return (
     <>
-      <h2>Erol Cetinok</h2>
-      <p>My Portfolio</p>
+      <h1>I'm Erol Cetinok, an aspiring mechanical engineer and roboticist</h1>
+      <h2>Welcome! This is a website (self-coded) to track my journey!</h2>
+
+      <p>
+        I am a current senior at Woodside Priory School and a leading mechanical
+        design member of{" "}
+        <a
+          href="https://team751.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Team 751, Barn2Robotics
+        </a>
+        .
+        I am also a full-stack (at least I like to think so) roboticist as well as a software engineering intern at {" "} 
+        <a
+          href="https://ceramic.ai/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ceramic.ai
+        </a>
+        .
+      </p>
     </>
   );
 }
