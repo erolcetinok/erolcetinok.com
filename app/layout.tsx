decb0e363ffc3dea19ad68e85cc08666743048e9
erolcetinok <erol.cetinok@gmail.com>
2026-02-09 00:00:30 -0800

    trying to work out vercel issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Inter, Source_Serif_4 } from "next/font/google";
 
 import Header from "@/components/Header";
-import { Providers } from "./providers";
+import { Providers } from "@/components/Providers";
 
 const inter = Inter({
   subsets: ["latin"],
